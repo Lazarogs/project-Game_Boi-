aaf32de44d6b496f83dfe21ca9cb236b711ef4f5
Guillermo <>
2019-12-01 18:19:55 +0100

    Premier commit!

---
+++ experiments/C/firstprog/Hello World!.c
@@ -0,0 +1,9 @@
+//Un programme pas optimisé, mais qui marche! Le tout début...
+
+#include <stdio.h>
+#include <gb/gb.h> //inclut la librairie qui nous permet de faire un port à la gameboy
+
+int main (void){
+	printf("Hello World!"); //on ne devrait pas utiliser printf car il bouffe toute la VRAM, mais c'est un programme d'éssais...
+	return 0;
+}
